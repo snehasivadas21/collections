9534ca948756d9e727f1a9767b2d5b11a8c828dd
snehasivadas21 <snehasivadas2002@gmail.com>
2025-07-24 17:29:48 +0530

    second commit

--- collections.py
+++ collections.py
@@ -9,20 +9,35 @@
 #     return s[0].upper()+s[1:-1]+s[-1].upper()
 #     return s.replace('a','w')
 #     return s.split()
-# print(rev("sneha is a cute"))    
+# print(rev("sneha is a cute"))  
 
-a=[1,2,3,4,"string"]
+def non_repeating(s):
+    d={}
+    for i in s:
+        if i in d:
+            d[i]+=1
+        else:
+            d[i]=1
+    for i in reversed(s):
+        if d[i]==1:
+            return i    
+print(non_repeating("abbc"))             
+
+# a=[1,2,3,4,"string"]
 # a.append(7)
 # a.extend([8,7])
 # a.insert(0,0)
 # a.sort(reverse=True)
 # print(a)
 # print(set(a))
-b=[x for x in a if isinstance(x,str)]
-print(b)
+# b=[x for x in a if isinstance(x,str)]
+# print(b)
 
 # l=[i**2 for i in range(1,6)]
 # print(l)
+
+even = [i for i in range(10) if i %2 ==0 ]
+print(even)
 
 d={"name":"sneha","age":23}
 # b={"a":1,"b":2}
@@ -35,6 +50,14 @@ print(d)
 # print(merged)
 for key,value in d.items():
     print(f"{key}-->{value}")
+
+squares = {z:z**2 for z in range(1,10)} 
+print(squares)   
+
+
+dic={1:"one","two":"two","three":"age"}
+output={k:v for k,v in dic.items() if isinstance(k,str)}
+print(output)
 
 # try:
 #     x=10/0
@@ -51,3 +74,14 @@ for key,value in d.items():
 #       break
 # else:
 #    print("loop completed")   
+
+import copy
+a=[[1,2],3]
+
+shallow=copy.copy(a)
+deep=copy.deepcopy(a)
+a[0][0]=9
+
+print(a)
+print(deep)
+print(shallow)
--- oops.py
+++ oops.py
@@ -1,13 +1,111 @@
-class Animal:
-    def speak(self):
-        return "sound"
-class Dog(Animal): # dog inherit from animal and override speak
-    def speak(self):
-        return "woof"
-dog=Dog()  
-print(dog.speak())  
+# class Animal:
+#     def speak(self):
+#         return "sound"
+# class Dog(Animal): # dog inherit from animal and override speak
+#     def speak(self):
+#         return "woof"
+# dog=Dog()  
+# print(dog.speak()) 
+
+# class Rectangle:
+#     def __init__(self,length,breadth):
+#         self.lenght=length
+#         self.breadth=breadth
+#     def area(self):
+#         return self.lenght * self.breadth
+# r=Rectangle(5,4)
+# print(r.area())        
+
+
+# from abc import ABC,abstractmethod
+# class Shape(ABC):
+#     @abstractmethod
+#     def area(self):
+#         pass
+# class Circle(Shape):
+#     def __init__(self,radius):
+#         self.radius=radius
+#     def area(self):
+#         return 3.14 * self.radius ** 2  
+# c=Circle(5)
+# print(c.area())
+
+# class Audio:
+#     def play(self):
+#         print("audio")
+# class Video:
+#     def play(slef):
+#         print("video")
+# def play_media(media):
+#     media.play()
+# audio=Audio()
+# video=Video()
+# play_media(audio)
+# play_media(video)                
 
 import random
-random_num=lambda : random.randint(1,10)
-print(random_num())
+# random_num=lambda : random.randint(1,10)
+# print(random_num())
+
+def rand_not_five():
+    random_num = random.randint(1,10)
+    while random_num==5:
+        random_num = random.randint(1,10)
+    return random_num
+print(rand_not_five())    
+ 
+# def count(n):
+#     for i in range(1,n+1):
+#         yield i
+# for z in count(5):
+#     print(z)      
+
+# def squares(n):
+#     for i in range(n):
+#         yield i**2
+# for i in squares(5):
+#     print(i)          
+
+
+# a=[1,2,3]
+# b=iter(a)
+# print(next(b))         
     
+# class Person:
+#     sleeptime =8
+#     @staticmethod
+#     def static_method():
+#         print(Person.sleeptime)
+#     @classmethod
+#     def class_method(cls):
+#         print(cls.sleeptime)
+# Person.static_method()
+# Person.class_method()     
+
+# def decorator(func):
+#     def wrapper():
+#         print("before")
+#         func()
+#         print("after")
+#     return wrapper
+# @decorator
+# def hello():
+#     print("hello world")
+# hello()        
+
+def decorator(func):
+    def wrapper(*args):
+        print(f"{func.__name__}")
+        return func(*args)
+    return  wrapper
+@decorator
+def add(a,b):
+   return a+b
+print(add(5,6))   
+
+import os
+with open("text.txt","w") as f:
+    f.write("hello")
+with open("text.txt","r") as f:
+    print(f.read())  
+os.remove("text.txt")    
